aa6208642d04d7b2606fb99073c76b267e6a6681
shemetm6 <shemetm6@gmail.com>
2026-01-19 15:29:05 +0500

    Add Docker support

--- CookBook/Contracts/Ingredient.cs
+++ CookBook/Contracts/Ingredient.cs
@@ -5,6 +5,6 @@ public class Ingredient
     public record IngredientVm(int Id, string Name, List<RecipeInIngredientVm> Recipes); 
     public record IngredientInListVm(int Id, string Name);
     public record RecipeInIngredientVm(int RecipeId, string RecipeTitle);
-    public record ListOfIngredients(IReadOnlyList<IngredientInListVm> Ingredients);
+    public record ListOfIngredients(List<IngredientInListVm> Ingredients);
     public record CreateIngredientDto(string Name);
 }
