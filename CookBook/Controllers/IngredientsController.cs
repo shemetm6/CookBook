17aac1e18e1aa4b2ef868176e68ef13fb8c2436b
shemetm6 <shemetm6@gmail.com>
2026-02-04 15:29:15 +0500

    feat: allow anonymous GET recipes and ingredients

--- CookBook/Controllers/IngredientsController.cs
+++ CookBook/Controllers/IngredientsController.cs
@@ -1,5 +1,6 @@
 ﻿using CookBook.Abstractions;
 using CookBook.Contracts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CookBook.Controllers;
@@ -14,11 +15,13 @@ public class IngredientsController : BaseController
     [HttpPost]
     public ActionResult<int> CreateIngredient(CreateIngredientDto dto) 
         => Ok(_ingredientService.AddIngredient(dto));
-    
+
+    [AllowAnonymous]
     [HttpGet]
     public ActionResult<ListOfIngredients> GetIngredients() 
         => Ok(_ingredientService.GetIngredients());
 
+    [AllowAnonymous]
     [HttpGet("{id}")]
     public ActionResult<IngredientVm> GetIngredient(int id)
         => Ok(_ingredientService.GetIngredient(id));
